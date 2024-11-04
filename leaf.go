94838c2d0a93b3174188a6ae934bb17943d730f8
Isaque Veras <isaqueveras@alu.ufc.br>
2024-11-04 20:50:14 -0300

    fix: add interface type

--- leaf.go
+++ leaf.go
@@ -7,13 +7,19 @@ import (
 	"github.com/isaqueveras/leaf/internal"
 )
 
+// IQueue defines the interface for the methods of a queue
+type IQueue internal.IQueue
+
 // New initialize a new queue
-func New(ctx context.Context, workers, itemsPerPage uint64, interval time.Duration) internal.IQueue {
+func New(ctx context.Context, workers, itemsPerPage uint64, interval time.Duration) IQueue {
 	return internal.New(ctx, workers, itemsPerPage, interval)
 }
 
+// IPage defines the methods of a page
+type IPage internal.IPage
+
 // GetPage get current page data
-func GetPage(ctx context.Context) internal.IPage {
+func GetPage(ctx context.Context) IPage {
 	return internal.GetPage(ctx)
 }
 
