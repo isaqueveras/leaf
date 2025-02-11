f2d237f8647a4faa33764f9a0e101fb866b7d962
Isaque Veras <isaqueveras@alu.ufc.br>
2025-02-11 10:56:59 -0300

    feat: add cursor-based pagination

--- internal/group.go
+++ internal/group.go
@@ -2,58 +2,41 @@ package internal
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"sync"
 )
 
 type group struct {
-	wg         *sync.WaitGroup
-	ctx        context.Context
-	cancelCtx  context.CancelFunc
-	errorMutex sync.Mutex
+	ctx context.Context
+	wg  sync.WaitGroup
 }
 
 type iGroup interface {
 	wrap(func() error) func()
 	wait()
-	cancel()
 }
 
 func newGroup(parent context.Context) iGroup {
-	return &group{ctx: parent, wg: &sync.WaitGroup{}}
-}
-
-func (g *group) cancel() {
-	g.cancelCtx()
+	return &group{ctx: parent, wg: sync.WaitGroup{}}
 }
 
 // wait for any packaged goroutine to finish and return an error that occurred
 func (g *group) wait() {
 	log.Println("Waiting on Group")
 	g.wg.Wait()
-
-	if g.cancelCtx != nil {
-		g.cancel()
-	}
 }
 
 // wraps a function to be executed in a goroutine
 func (g *group) wrap(fn func() error) func() {
 	g.wg.Add(1)
-
 	return func() {
 		defer g.wg.Done()
-		_, g.cancelCtx = context.WithCancel(g.ctx)
+
+		_, cancel := context.WithCancel(g.ctx)
+		defer cancel()
 
 		if err := fn(); err != nil {
-			g.errorMutex.Lock()
-			fmt.Printf("Error: %v\n", err.Error())
-			if g.cancelCtx != nil {
-				log.Println("Group canceling its context due to error")
-				g.cancel()
-			}
-			g.errorMutex.Unlock()
+			log.Println("Group canceling its context due to error: ", err.Error())
 		}
 	}
 }
--- internal/page.go
+++ internal/page.go
@@ -9,6 +9,11 @@ type IPage interface {
 	// GetItemsPerPage get the value of the quantity of items per page
 	GetItemsPerPage() uint64
 
+	// GetStartCursor returns the starting ID of the current page in cursor-based pagination.
+	GetStartCursor() uint64
+	// GetEndCursor returns the ending ID of the current page in cursor-based pagination.
+	GetEndCursor() uint64
+
 	getPage() page
 	calculate()
 }
@@ -17,32 +22,36 @@ type page struct {
 	page         uint64
 	itemsPerPage uint64
 	offset       uint64
+
+	cursorStart uint64
+	cursorEnd   uint64
 }
 
 func newPage(itemsPerPage uint64) IPage {
 	return &page{itemsPerPage: itemsPerPage}
 }
 
+// GetStartCursor returns the starting ID of the current page in cursor-based pagination.
+func (p *page) GetStartCursor() uint64 { return p.cursorStart }
+
+// GetEndCursor returns the ending ID of the current page in cursor-based pagination.
+func (p *page) GetEndCursor() uint64 { return p.cursorEnd }
+
 // GetOffset get the offset of the current page
-func (p *page) GetOffset() uint64 {
-	return p.offset
-}
+func (p *page) GetOffset() uint64 { return p.offset }
 
 // GetItemsPerPage get the value of the quantity of items per page
-func (p *page) GetItemsPerPage() uint64 {
-	return p.itemsPerPage
-}
+func (p *page) GetItemsPerPage() uint64 { return p.itemsPerPage }
 
 // GetCurrentPage get the value of the current page
-func (p *page) GetCurrentPage() uint64 {
-	return p.page
-}
+func (p *page) GetCurrentPage() uint64 { return p.page }
+
+func (p *page) getPage() page { return *p }
 
 func (p *page) calculate() {
 	p.page++
 	p.offset = (p.page - 1) * p.itemsPerPage
-}
 
-func (p *page) getPage() page {
-	return *p
+	p.cursorStart = p.cursorEnd
+	p.cursorEnd += p.itemsPerPage
 }
